e06f5741ef8158247ef9cef9d38f31c9b738ba76
Linusdalin <linus.dalin@itclarifies.com>
2015-08-22 13:15:02 +0200

    This is a test commit

--- src/core/Execute.java
+++ src/core/Execute.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 /************************************************************************************
  *
  *              Execute is the main class for running campaigns
+ *
+ *              This will both produce and consume
  */
 
 public class Execute {
